fcee723d5178c6f66beafad139bad87fa6186a29
Rajinder Mavi <rajinder@mavi.phd>
2026-01-05 21:53:14 -0800

    init for modules

---
+++ send/__init__.py
@@ -0,0 +1,3 @@
+from .client import EmailClient
+
+__all__ = ["EmailClient"]
---
+++ send/message/__init__.py
@@ -0,0 +1,4 @@
+from .builder import EmailMessageBuilder
+from .models import Attachment
+
+__all__ = ["EmailMessageBuilder", "Attachment"]
